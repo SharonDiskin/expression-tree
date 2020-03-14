eaa4a356ea4875bc8bcbe3c2d1f53ee5520409cd
SharonDiskin <40810071+SharonDiskin@users.noreply.github.com>
2020-03-14 02:26:21 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "expressionTree.h"
+
+void main()
+
+{
+	char str[SIZE];
+	Tree tr;
+	double res;
+	BOOL expressionOK;
+
+	printf("Please enter the expression: ");
+	gets(str);
+	expressionOK = buildExpressionTree(str, &tr);
+	if (expressionOK == TRUE)
+	{
+		res = calcExpression(tr);
+		printf("%s = %.2f", str, res);
+	}
+
+	else
+		printf("The input expression is not valid\n");
+	freeTree(tr);
+}
